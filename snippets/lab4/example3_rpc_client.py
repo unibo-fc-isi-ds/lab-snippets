6e2f8c0ba31f037e8b61111b3f518be89c77c2ef
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2024-10-08 17:17:46 +0200

    feat: programmatic and cli example for rpc client

--- snippets/lab4/example3_rpc_client.py
+++ snippets/lab4/example3_rpc_client.py
@@ -3,81 +3,75 @@ from snippets.lab4.users import *
 from snippets.lab4.example1_presentation import serialize, deserialize, Request, Response
 
 
-class ClientStub(Client):
-    def __init__(self, server_address):
-        super().__init__(server_address)
-        print('# Connected to %s:%d' % self.remote_address)
-    
+class ClientStub:
+    def __init__(self, server_address: tuple[str, int]):
+        self.__server_address = address(*server_address)
+
     def rpc(self, name, *args):
-        request = Request(name, args)
-        print('# Marshalling', request, 'towards', "%s:%d" % self.remote_address)
-        request = serialize(request)
-        print('# Sending message:', request.replace('\n', '\n# '))
-        self.send(request)
-        response = self.receive()
-        print('# Received message:', response.replace('\n', '\n# '))
-        response = deserialize(response)
-        assert isinstance(response, Response)
-        print('# Unmarshalled', response, 'from', "%s:%d" % self.remote_address)
-        if response.error:
-            raise RuntimeError(response.error)
-        return response.result
-    
-    def close(self):
-        super().close()
-        print('# Disconnected from %s:%d' % self.remote_address)
+        client = Client(self.__server_address)
+        try:
+            print('# Connected to %s:%d' % client.remote_address)
+            request = Request(name, args)
+            print('# Marshalling', request, 'towards', "%s:%d" % client.remote_address)
+            request = serialize(request)
+            print('# Sending message:', request.replace('\n', '\n# '))
+            client.send(request)
+            response = client.receive()
+            print('# Received message:', response.replace('\n', '\n# '))
+            response = deserialize(response)
+            assert isinstance(response, Response)
+            print('# Unmarshalled', response, 'from', "%s:%d" % client.remote_address)
+            if response.error:
+                raise RuntimeError(response.error)
+            return response.result
+        finally:
+            client.close()
+            print('# Disconnected from %s:%d' % client.remote_address)
 
 
 class RemoteUserDatabase(ClientStub, UserDatabase):
     def __init__(self, server_address):
         super().__init__(server_address)
-    
+
     def add_user(self, user: User):
         return self.rpc('add_user', user)
-    
+
     def get_user(self, id: str) -> User:
         return self.rpc('get_user', id)
-    
+
     def check_password(self, credentials: Credentials) -> bool:
         return self.rpc('check_password', credentials)
-    
+
 
 if __name__ == '__main__':
-    import argparse
+    from snippets.lab4.example0_users import gc_user, gc_credentials_ok, gc_credentials_wrong
+    import sys
 
-    parser = argparse.ArgumentParser(description='RPC client for user database')
-    parser.add_argument('address', help='Server address in the form ip:port')
-    parser.add_argument('command', help='Method to call', choices=['add', 'get', 'check'])
-    parser.add_argument('--user', '-u', help='Username')
-    parser.add_argument('--email', '--address', '-a', nargs='+', help='Email address')
-    parser.add_argument('--name', '-n', help='Full name')
-    parser.add_argument('--role', '-r', help='Role (defaults to "user")', choices=['admin', 'user'])
-    parser.add_argument('--password', '-p', help='Password')
 
-    args = parser.parse_args()
-    args.address = address(args.address)
-    user_db = RemoteUserDatabase(args.address)
+    user_db = RemoteUserDatabase(address(sys.argv[1]))
 
-    try :
-        ids = (args.email or []) + [args.user]
-        if len(ids) == 0:
-            raise ValueError("Username or email address is required")
-        match args.command:
-            case 'add':
-                if not args.password:
-                    raise ValueError("Password is required")
-                if not args.name:
-                    raise ValueError("Full name is required")
-                user = User(args.user, args.email, args.name, Role[args.role.upper()], args.password)
-                print(user_db.add_user(user))
-            case 'get':
-                print(user_db.get_user(ids[0]))
-            case 'check':
-                credentials = Credentials(ids[0], args.password)
-                print(user_db.check_password(credentials))
-            case _:
-                raise ValueError(f"Invalid command '{args.command}'")
+    # Trying to get a user that does not exist should raise a KeyError
+    try:
+        user_db.get_user('gciatto')
     except RuntimeError as e:
-        print(f'[{type(e).__name__}]', *e.args)
-    finally:
-        user_db.close()
+        assert 'User with ID gciatto not found' in str(e)
+
+    # Adding a novel user should work
+    user_db.add_user(gc_user)
+
+    # Trying to add a user that already exist should raise a ValueError
+    try:
+        user_db.add_user(gc_user)
+    except RuntimeError as e:
+        assert str(e).startswith('User with ID')
+        assert str(e).endswith('already exists')
+
+    # Getting a user that exists should work
+    assert user_db.get_user('gciatto') == gc_user.copy(password=None)
+
+    # Checking credentials should work if there exists a user with the same ID and password (no matter which ID is used)
+    for gc_cred in gc_credentials_ok:
+        assert user_db.check_password(gc_cred) == True
+
+    # Checking credentials should fail if the password is wrong
+    assert user_db.check_password(gc_credentials_wrong) == False
---
+++ snippets/lab4/example4_rpc_client_cli.py
@@ -0,0 +1,50 @@
+from .example3_rpc_client import *
+import argparse
+import sys
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        prog=f'python -m snippets -l 4 -e 4',
+        description='RPC client for user database',
+        exit_on_error=False,
+    )
+    parser.add_argument('address', help='Server address in the form ip:port')
+    parser.add_argument('command', help='Method to call', choices=['add', 'get', 'check'])
+    parser.add_argument('--user', '-u', help='Username')
+    parser.add_argument('--email', '--address', '-a', nargs='+', help='Email address')
+    parser.add_argument('--name', '-n', help='Full name')
+    parser.add_argument('--role', '-r', help='Role (defaults to "user")', choices=['admin', 'user'])
+    parser.add_argument('--password', '-p', help='Password')
+
+    if len(sys.argv) > 1:
+        args = parser.parse_args()
+    else:
+        parser.print_help()
+        sys.exit(0)
+
+    args.address = address(args.address)
+    user_db = RemoteUserDatabase(args.address)
+
+    try :
+        ids = (args.email or []) + [args.user]
+        if len(ids) == 0:
+            raise ValueError("Username or email address is required")
+        match args.command:
+            case 'add':
+                if not args.password:
+                    raise ValueError("Password is required")
+                if not args.name:
+                    raise ValueError("Full name is required")
+                user = User(args.user, args.email, args.name, Role[args.role.upper()], args.password)
+                print(user_db.add_user(user))
+            case 'get':
+                print(user_db.get_user(ids[0]))
+            case 'check':
+                credentials = Credentials(ids[0], args.password)
+                print(user_db.check_password(credentials))
+            case _:
+                raise ValueError(f"Invalid command '{args.command}'")
+    except RuntimeError as e:
+        print(f'[{type(e).__name__}]', *e.args)
