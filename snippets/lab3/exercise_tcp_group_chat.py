4f062ea873a3a53589c12d2098335b581968a5a8
Pepe Riccardo <riccardo.pepe3@studio.unibo.it>
2024-11-20 02:18:26 +0100

    Now Peers can establish connection between them. Deleted client draft class. In ip check now localhost is the same of 127.0.0.1

--- snippets/lab3/exercise_tcp_group_chat.py
+++ snippets/lab3/exercise_tcp_group_chat.py
@@ -8,6 +8,15 @@ import socket
 import sys
 import json
 import logging
+import time
+import psutil
+
+
+def local_ips():
+    for interface in psutil.net_if_addrs().values():
+        for addr in interface:
+            if addr.family == socket.AF_INET:
+                    yield addr.address
 
 # Functions
 def validateIpAddress(ip: str):
@@ -35,8 +44,12 @@ def obtainIpaddressFromString(ip='0.0.0.0:0', port=None):
         port = 0
     if (port not in range(0, 65536)):
         raise InvalidPortRange
-    if (not isinstance(ip, str) or not validateIpAddress(ip)):
+    if (not isinstance(ip, str)):
+        raise InvalidIpAddress("Ip is not a string")
+    if (not validateIpAddress(ip)):
         raise InvalidIpAddress
+    if (ip == 'localhost'):
+        ip = '127.0.0.1'
     return ip, port
 
 
@@ -99,84 +112,69 @@ class Message():
 
 # Può essere utile avere uno strumento di log
 class Peer():
-    def __init__(self, port: int, peers = None, log: bool = False):
+    LOG_FILE_NAME = "peer.log"
+    BUFFER_SIZE = 2048
+    CLOSED_CONNECTION_REQUEST = "$$$EXIT"
+    NEW_CONNECTION_REQUEST = "$$$NEWCONNECT"
+    SELF_IP_ADDRESS = "127.0.0.1"
+    MAX_NUMBER_LISTENER = 100
+
+    def __init__(self, ip: str, port: int, peers = None, log: bool = False):
         self.__thread = []
         self.__observer = []
         self.__logger = log
         self.port = port
         self.__username = None
-        
-        if (self.__logger):
-            logging.basicConfig(filename=("peer.log"),
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    filemode='w')
-            self.__logger = logging.getLogger()
-            self.__logger.setLevel(logging.DEBUG)
+        self.__connections = {}
+        self.ip = ip
 
-        self.__set_peers(peers)
-        
-        if (self.__logger):
-            self.__logger.info((str(self.port) if not self.__isUsernameSet() 
-                                else "{"+self.__username+"}") 
-                               + " Peers: " + str(self.peers))
+        self.__startLogger()
 
-        self.__inputUsername()
-        
-        self.server_thread = threading.Thread(target=self.__serverStart, args=([port]))
-        self.server_thread.start()
-
-    def __isUsernameSet(self):
-        if (self.__username == None):
-            return False
-        else:
-            return True
-
-    def __inputUsername(self):
-        print("Choose an username for the chat: ")
-        self.__username = input()
-        if (self.__logger):
-            self.__logger.info((str(self.port) if not self.__isUsernameSet() 
-                                else "{"+self.__username+"}")
-                                  + " Usarname: " + self.__username)
-
-    def __serverStart(self, port):
         try:
-            self.server_thread = threading.Thread(target=self.__set_server, args=([port]))
-            self.server_thread.daemon = True
-            self.server_thread.start()
-            self.server_thread.join()
-        except KeyboardInterrupt:
-            self.shutdown()
-            for thread in self.__thread:
-                thread.join()
-            self.close()
-            if (self.__logger):
-                self.__logger.info((str(self.port) if not self.__isUsernameSet() 
-                                else "{"+self.__username+"}")
-                                 + " Server socket closed")
-            sys.exit(0)
+            self.ip, _ = obtainIpaddressFromString(self.ip)
+            if self.ip not in list(local_ips()):
+                self.__logError("Ip in args is not compatible")
+                sys.exit(1)
+        except (InvalidIpAddress):
+            self.__logError("There is no ip in args")
+            sys.exit(1)
+        
+        self.__set_peers(peers)
+        self.__logger.info("Peers: " + str(self.peers))
 
-    def shutdown(self):
-        self.server_thread.join()
+    def start(self):        
+        self.server_thread = threading.Thread(target=self.__serverStart, args=([self.port]))
+        self.server_thread.start()
+        self.__connectToAllPeers()
+
+    def inputUsername(self, name: str):
+        self.__username = name
+
+    def sendToEveryone(self, message: str):
+        for receiver in self.peers:
+            self.send(receiver[0], receiver[1], message)
             
-    def send(self, message: Message):
-        pass
+    def send(self, ip: str, port: int, message: str):
+        try:
+            self.__connections[(ip, port)].sendall(bytes(message, 'utf-8'))
+            self.__logInfo("Send message <" + message + "> to " + ip + ":" + str(port))
+        except Exception as e:
+            self.__logError("Error send message: " + str(e))
 
-    def receive(self,conn,addr):
+    def receive(self, conn):
         while True:
-            message = conn.recv(2048) 
-            message_str = message.decode('utf-8')
-            if (self.__logger):
-                    self.__logger.info((str(self.port) if not self.__isUsernameSet() 
-                            else "{"+self.__username+"}") + " Received message: " + message_str)
-            m = Message(message_str)
-            self.notify(m)
-            if(len(m.originalData["message"]) > 0 and m.originalData["message"] == '$$$EXIT'):
-                if (self.__logger):
-                    self.__logger.info((str(self.port) if not self.__isUsernameSet() 
-                                else "{"+self.__username+"}") + " Closed connection")
-                conn.close()
-                return
+            message = conn.recv(self.BUFFER_SIZE).decode('utf-8')
+            if (len(message) > 0):
+                self.__logInfo("Received message <" + message + "> from ip: " 
+                               + str(conn.getpeername()[0]) + " port: " + str(conn.getpeername()[1]))
+                m = Message(message)
+                self.notify(m)
+                if(self.__isCloseConnectionMessage(m.originalData["message"])):
+                    self.__logInfo("Closed connection")
+                    conn.close()
+                    return
+                if(self.__isNewConnectionMessage(m)):
+                    self.__acceptNewConnection(m)
             
     def addObserver(self, observer):
         self.__observer.append(observer)
@@ -185,58 +183,129 @@ class Peer():
         for singleObserver in self.__observer:
             singleObserver.handleOutputMessage(message)
 
-    def connect(self, peers: list['Peer']):
-        pass
+    def connect(self, ip: str, port: int):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            client.connect((ip, port))
+            self.__connections[(ip, port)] = client
+            self.send(ip, port, Message(self.__newConnectionMessage()).encodedData)
+            print("Connected with ip: " + ip + " port: " + str(port))
+            self.__logInfo("Connected with ip: " + ip + " port: " + str(port))
+        except OSError:
+            self.__logError("Can not connect with ip: " + ip + " port: " + str(port))
+                    
     def disconnect(self, peers: list['Peer']):
         pass
 
     def close(self):
         if self.__socket:
             self.__socket.close()
+        for key, value in self.__connections.items():
+            value.close()
         for i in self.__thread:
             i.join()
+        self.server_thread.join()
+
+    def __connectToAllPeers(self):
+        for addr, port in self.peers:
+            addr, port = obtainIpaddressFromString(addr, port)
+            self.connect(addr, port)
+
+    def __newConnectionMessage(self):
+        return {
+            "serverIP": self.ip,
+            "serverPort": self.port,
+            "username": self.__username if self.__isUsernameSet() 
+                else (self.ip + ":" + str(self.port)),
+            "message": self.NEW_CONNECTION_REQUEST
+        }
+
+    def __acceptNewConnection(self, message: Message):
+        ip, port = obtainIpaddressFromString(message.originalData["serverIP"], 
+                                               port = message.originalData["serverPort"])
+        self.connect(ip, port)
+
+    def __isNewConnectionMessage(self, message: Message):
+        return message.originalData["message"] == self.NEW_CONNECTION_REQUEST and not ((message.originalData["serverIP"], message.originalData["serverPort"]) in self.peers)
+
+    def __isCloseConnectionMessage(self, message):
+        return message == self.CLOSED_CONNECTION_REQUEST
+
+    def __serverStart(self, port):
+        try:
+            self.server_thread = threading.Thread(target=self.__set_server, args=([port]))
+            self.server_thread.daemon = True
+            self.server_thread.start()
+            self.server_thread.join()
+        except KeyboardInterrupt:
+            for thread in self.__thread:
+                thread.join()
+            self.close()
+            self.__logInfo("Server socket closed")
+            sys.exit(0)
 
     def __set_server(self, port):
         self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            self.__socket.bind(obtainIpaddressFromString('127.0.0.1', port=port))
+            self.__socket.bind(obtainIpaddressFromString(self.SELF_IP_ADDRESS, port=port))
         except (InvalidPortRange, InvalidIpAddress) as e:
-            print(f"Error: {e}")
-            if (self.__logger):
-                self.__logger.error((str(self.port) if not self.__isUsernameSet() 
-                                else "{"+self.__username+"}") + e)
+            self.__logError(e)
             sys.exit(1)
-        self.__socket.listen(100)
+        self.__socket.listen(self.MAX_NUMBER_LISTENER)
         while True:
-            conn, addr = self.__socket.accept()  # Accept a client connection
-            thread = threading.Thread(target=self.receive, args=(conn, addr))
+            conn, _ = self.__socket.accept()  # Accept a client connection
+            thread = threading.Thread(target=self.receive, args=([conn]))
             thread.daemon = True
             self.__thread.append(thread)
             thread.start()  # Start a new thread for each client
-            if (self.__logger):
-                self.__logger.info((str(self.port) if not self.__isUsernameSet() 
-                        else "{"+self.__username+"}") + " New connection active")
+            self.__logInfo("New connection active from ip: " + str(conn.getpeername()[0]) + " port: " + str(conn.getpeername()[1]))
 
     def __set_peers(self, peers):
         if peers is None:
             peers = set()
         try:
-            self.peers = {obtainIpaddressFromString(*peer) for peer in peers}
+            self.peers = {obtainIpaddressFromString(peer) for peer in peers}
         except (InvalidPortRange, InvalidIpAddress) as e:
-            print(f"Error: {e}")
-            if (self.__logger):
-                self.__logger.error((str(self.port) if not self.__isUsernameSet() 
-                                else "{"+self.__username+"}") + e)
+            self.__logError(e)
             sys.exit(1)
+
+    def __logError(self, message):
+        if (self.__logger):
+                self.__logger.error((str(self.port) if not self.__isUsernameSet() 
+                                else "{"+self.__username+"} ") + message)
+                
+    def __logInfo(self, message):
+        if (self.__logger):
+                self.__logger.info((str(self.port) if not self.__isUsernameSet() 
+                                else "{"+self.__username+"} ") + message)
+                
+    def __isUsernameSet(self):
+        if (self.__username == None):
+            return False
+        else:
+            return True
+        
+    def __startLogger(self):
+        if (self.__logger):
+            logging.basicConfig(filename=(self.LOG_FILE_NAME),
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filemode='w')
+            self.__logger = logging.getLogger()
+            self.__logger.setLevel(logging.DEBUG)
     
     # funzione privata periodica di controllo della rete
 
 
 class Controller():
+    def __init__(self):
+        self.__observer = []
+    def addObserver(self, observer):
+        self.__observer.append(observer)
     def handleOutputMessage(self, message: Message):
-        print(message.originalData["sender"] + " " + message.originalData["message"])
+        print(message.originalData["username"] + " " + message.originalData["message"])
     def handleInputMessage(self, message: Message):
-        pass
+        for singlebserver in self.__observer:
+            singlebserver.sendToEveryone(message.encodedData)
 
 class View():
     def outputMessage(self, message: str):
@@ -244,54 +313,7 @@ class View():
     def inputMessage(self) -> str:
         pass
 
-class Client:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        
-    def invia_messaggi(self,port):
-        try:
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            time.sleep(10)
-            client.connect((self.host, port))
-            time.sleep(2)
-            
-            strl= {
-                "sender": "Carlo",
-                "message": "Ciao"
-            }
-            client.send(bytes(Message(strl).encodedData, 'utf-8'))
-            time.sleep(2)
-            strl= {
-                "sender": "Carlo",
-                "message": "Ciao2"
-            }
-            client.send(bytes(Message(strl).encodedData, 'utf-8'))
-            time.sleep(2)
-            strl= {
-                "sender": "Carlo",
-                "message": "$$$EXIT"
-            }
-            client.send(bytes(Message(strl).encodedData, 'utf-8'))
-            client.close()
-
-        except KeyboardInterrupt:
-            print("E' qui")
-
-    def start(self):
-        self.invia_thread = []
-        for i in range(len(self.port)):
-            single = threading.Thread(target=self.invia_messaggi, args=([self.port[i]]))
-            self.invia_thread.append(single)
-            single.daemon = True
-            single.start()
-            
-        for j in self.invia_thread:
-            j.join()
-
-import time
-
-# TODO integrare client in peer
+# TODO aggiungere disconnesione da rete
 # TODO creare test peer
 # TODO sistemare Exception
 # TODO refactoring codice
@@ -299,25 +321,31 @@ import time
 # TODO ordinare codice
 # TODO controllare se tutti i thread sono necessari
 # TODO associare connessioni con thread
+# TODO controllare concorrenza
 
 if __name__=='__main__':
     try:
-        cl = Client('localhost',[8081,8082])
-        cl_thread = threading.Thread(target=cl.start)
-        cl_thread.daemon = True
-        cl_thread.start()
+        peer2 = Peer('127.0.0.1', 8082, peers=None, log=True)
+        peer1 = Peer('127.0.0.1', 8081, peers=["localhost:8082"], log=True)
         c = Controller()
-        peer1 = Peer(8081, peers=None, log=True)
-        peer2 = Peer(8082, peers=None, log=True)
         peer1.addObserver(c)
         peer2.addObserver(c)
-        cl_thread.join()
+        c.addObserver(peer1)
+        print("Inserire Username:")
+        peer1.inputUsername(input())
+        print("Inserire Username:")
+        peer2.inputUsername(input())
+        peer2.start()
+        peer1.start()
+        time.sleep(5)
+        strl= {
+            "username": "Carlo",
+            "message": "Ciao"
+        }
+        c.handleInputMessage(Message(strl))
     except KeyboardInterrupt:
         peer1.shutdown()
         peer2.shutdown()
-        for j in cl.invia_thread:
-            j.join()
-
 
 
 class Test():
