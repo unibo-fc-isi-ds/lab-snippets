e4b64f8e3727bb1ba0189b285e77680e6d3b5fcf
Sentinel <muhamad.salman2001@gmail.com>
2024-11-21 12:07:51 +0100

    added es4-1 rpc auth

--- snippets/lab4/example4_rpc_client_cli.py
+++ snippets/lab4/example4_rpc_client_cli.py
@@ -11,12 +11,13 @@ if __name__ == '__main__':
         exit_on_error=False,
     )
     parser.add_argument('address', help='Server address in the form ip:port')
-    parser.add_argument('command', help='Method to call', choices=['add', 'get', 'check'])
+    parser.add_argument('command', help='Method to call', choices=['add', 'get', 'check', 'authenticate', 'validate'])
     parser.add_argument('--user', '-u', help='Username')
     parser.add_argument('--email', '--address', '-a', nargs='+', help='Email address')
     parser.add_argument('--name', '-n', help='Full name')
     parser.add_argument('--role', '-r', help='Role (defaults to "user")', choices=['admin', 'user'])
     parser.add_argument('--password', '-p', help='Password')
+    parser.add_argument('--token', '-t', help='Token')
 
     if len(sys.argv) > 1:
         args = parser.parse_args()
@@ -26,6 +27,7 @@ if __name__ == '__main__':
 
     args.address = address(args.address)
     user_db = RemoteUserDatabase(args.address)
+    auth_service = RemoteAuthenticationService(args.address)
 
     try :
         ids = (args.email or []) + [args.user]
@@ -44,6 +46,12 @@ if __name__ == '__main__':
             case 'check':
                 credentials = Credentials(ids[0], args.password)
                 print(user_db.check_password(credentials))
+            case 'authenticate':
+                credentials = Credentials(args.user, args.password)
+                print(auth_service.authenticate(credentials))
+            case 'validate':
+                token = Token(User(args.user, args.email, args.name, Role[args.role.upper()]), datetime.now(), args.token)
+                print(auth_service.validate_token(token))
             case _:
                 raise ValueError(f"Invalid command '{args.command}'")
     except RuntimeError as e:
