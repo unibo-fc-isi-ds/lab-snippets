b4d91b1968860d6a61c7e3930d8a3bda133e2219
Nicolò Monaldini <nicolo.monaldini@gmail.com>
2024-11-23 14:56:05 +0100

    feat: Added RPC exercises

--- snippets/lab4/example5_secure_rpc_server.py
+++ snippets/lab4/example5_secure_rpc_server.py
@@ -1,14 +1,16 @@
 from snippets.lab3 import Server
+from snippets.lab4.users import Role
 from snippets.lab4.users.impl import InMemoryUserDatabase, InMemoryAuthenticationService
-from snippets.lab4.example1_presentation import serialize, deserialize, Request, Response
+from snippets.lab4.example1_presentation import SecureRequest, serialize, deserialize, Response
 import traceback
 
 
-class ServerStub(Server):
+class SecureServerStub(Server):
     def __init__(self, port):
         super().__init__(port, self.__on_connection_event)
         self.__user_db = InMemoryUserDatabase()
         self.__auth_service = InMemoryAuthenticationService(self.__user_db)
+        self.__secure_methods = {'get_user'}
     
     def __on_connection_event(self, event, connection, address, error):
         match event:
@@ -26,7 +28,7 @@ class ServerStub(Server):
             case 'message':
                 print('[%s:%d] Open connection' % connection.remote_address)
                 request = deserialize(payload)
-                assert isinstance(request, Request)
+                assert isinstance(request, SecureRequest)
                 print('[%s:%d] Unmarshall request:' % connection.remote_address, request)
                 response = self.__handle_request(request)
                 connection.send(serialize(response))
@@ -39,6 +41,15 @@ class ServerStub(Server):
     
     def __handle_request(self, request):
         try:
+            if (
+                request.name in self.__secure_methods and 
+                (
+                    not request.metadata or 
+                    request.metadata.user.role != Role.ADMIN or 
+                    not self.__auth_service.validate_token(request.metadata)
+                )
+            ):
+                raise PermissionError(f"Secure operation '{request.name}' requires admin privileges")
             if hasattr(self.__user_db, request.name):
                 method = getattr(self.__user_db, request.name)
             elif hasattr(self.__auth_service, request.name):
@@ -55,7 +66,7 @@ class ServerStub(Server):
 
 if __name__ == '__main__':
     import sys
-    server = ServerStub(int(sys.argv[1]))
+    server = SecureServerStub(int(sys.argv[1]))
     while True:
         try:
             input('Close server with Ctrl+D (Unix) or Ctrl+Z (Win)\n')
