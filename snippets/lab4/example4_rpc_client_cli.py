8f64b3ede81ec0c16b6853d45a05ea6e1c2c8aeb
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2024-11-25 17:43:40 +0100

    Initial commit, first implementation of exercise-lab4

--- snippets/lab4/example4_rpc_client_cli.py
+++ snippets/lab4/example4_rpc_client_cli.py
@@ -1,6 +1,7 @@
 from .example3_rpc_client import *
 import argparse
 import sys
+from datetime import timedelta
 
 
 if __name__ == '__main__':
@@ -11,12 +12,14 @@ if __name__ == '__main__':
         exit_on_error=False,
     )
     parser.add_argument('address', help='Server address in the form ip:port')
-    parser.add_argument('command', help='Method to call', choices=['add', 'get', 'check'])
+    parser.add_argument('command', help='Method to call', choices=['add', 'get', 'check', 'authenticate', 'validate'])
     parser.add_argument('--user', '-u', help='Username')
     parser.add_argument('--email', '--address', '-a', nargs='+', help='Email address')
     parser.add_argument('--name', '-n', help='Full name')
     parser.add_argument('--role', '-r', help='Role (defaults to "user")', choices=['admin', 'user'])
     parser.add_argument('--password', '-p', help='Password')
+    parser.add_argument('--tokenexpiration', '-x', help='Token expiration')
+    parser.add_argument('--tokensign', '-s', help='Token signature')
 
     if len(sys.argv) > 1:
         args = parser.parse_args()
@@ -26,6 +29,7 @@ if __name__ == '__main__':
 
     args.address = address(args.address)
     user_db = RemoteUserDatabase(args.address)
+    auth_service = RemoteAuthenticationService(args.address)
 
     try :
         ids = (args.email or []) + [args.user]
@@ -44,6 +48,17 @@ if __name__ == '__main__':
             case 'check':
                 credentials = Credentials(ids[0], args.password)
                 print(user_db.check_password(credentials))
+            case 'authenticate':
+                credentials = Credentials(ids[0], args.password)
+                token = auth_service.authenticate(credentials)
+                print(f"Token expiration: '{token.expiration.isoformat()}', token signature: '{token.signature}'")
+            case 'validate':
+                if not args.tokenexpiration:
+                    raise ValueError("Token expiration is required")
+                if not args.tokensign:
+                    raise ValueError("Token signature is required")
+                token = Token(user_db.get_user(ids[0]), datetime.fromisoformat(args.tokenexpiration), args.tokensign)
+                print(auth_service.validate_token(token))
             case _:
                 raise ValueError(f"Invalid command '{args.command}'")
     except RuntimeError as e:
