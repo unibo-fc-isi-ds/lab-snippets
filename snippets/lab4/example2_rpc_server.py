92b99e6d483e3473e1cdfaf8390051870dc54f18
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2024-11-28 12:33:04 +0100

    Added checks for authentication when necessary, removed Exception handling to ease debugging

--- snippets/lab4/example2_rpc_server.py
+++ snippets/lab4/example2_rpc_server.py
@@ -1,5 +1,5 @@
 from snippets.lab3 import Server
-from snippets.lab4.users.impl import InMemoryUserDatabase
+from snippets.lab4.users.impl import InMemoryUserDatabase, InMemoryAuthenticationService
 from snippets.lab4.example1_presentation import serialize, deserialize, Request, Response
 import traceback
 
@@ -8,6 +8,7 @@ class ServerStub(Server):
     def __init__(self, port):
         super().__init__(port, self.__on_connection_event)
         self.__user_db = InMemoryUserDatabase()
+        self.__auth_db = InMemoryAuthenticationService(database=self.__user_db, secret=None, debug=True)
     
     def __on_connection_event(self, event, connection, address, error):
         match event:
@@ -36,15 +37,35 @@ class ServerStub(Server):
             case 'close':
                 print('[%s:%d] Close connection' % connection.remote_address)
     
-    def __handle_request(self, request):
-        try:
-            method = getattr(self.__user_db, request.name)
-            result = method(*request.args)
-            error = None
-        except Exception as e:
-            result = None
-            error = " ".join(e.args)
-        return Response(result, error)
+    def __handle_request(self, request:Request): # this is where the actual function to execute is selecte and executed
+        #try:
+            #raise Exception(request.name)
+            if(request.name=='authenticate'):
+                result=self.__auth_db.authenticate(*request.args)
+                #e se autenticazione non valida?
+                error=None
+            else:
+                if(request.name!='add_user'):
+                    #the token is needed
+                    if(request.metadata!=None and self.__auth_db.validate_token(request.metadata)):
+                        #the token is valid
+                        method = getattr(self.__user_db, request.name) # get the method from the user database
+                        result = method(*request.args) # call the method with the arguments, store the result
+                        error = None # no error occurred in this case
+                    else:
+                        #the token is not valid
+                        result=None
+                        error="Invalid authentication token"
+                else:
+                #the token is not needed for this operation
+                    method = getattr(self.__user_db, request.name) # get the method from the user database
+                    result = method(*request.args) # call the method with the arguments, store the result
+                    error = None # no error occurred in this case
+
+        #except Exception as e: # if an error occurs:
+        #    result = None # no result in case of an error
+        #    error = " ".join(e.args) # store the error message
+            return Response(result, error) # return a Response object with the result and the error message
 
 
 if __name__ == '__main__':
