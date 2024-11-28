42b0b30cbeb6c402db3d3f02775d8462ddfe99bd
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2024-11-28 09:17:00 +0100

    Added authenticate command in client files

--- snippets/lab4/example3_rpc_client.py
+++ snippets/lab4/example3_rpc_client.py
@@ -41,6 +41,9 @@ class RemoteUserDatabase(ClientStub, UserDatabase):
 
     def check_password(self, credentials: Credentials) -> bool:
         return self.rpc('check_password', credentials)
+    
+    def authenticate(self, credentials: Credentials) -> Token:
+        return self.rpc('authenticate', credentials)
 
 
 if __name__ == '__main__':
