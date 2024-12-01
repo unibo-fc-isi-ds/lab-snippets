1ff719adb1ab24272c8c60fadb84e578ca928bfa
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2024-12-01 12:43:31 +0100

    Implemented authenticate, validate and delete_token commands

--- snippets/lab4/example4_rpc_client_cli.py
+++ snippets/lab4/example4_rpc_client_cli.py
@@ -1,5 +1,6 @@
 from .example3_rpc_client import *
 import argparse
+import os
 import sys
 
 
@@ -11,7 +12,7 @@ if __name__ == '__main__':
         exit_on_error=False,
     )
     parser.add_argument('address', help='Server address in the form ip:port')
-    parser.add_argument('command', help='Method to call', choices=['add', 'get', 'check'])
+    parser.add_argument('command', help='Method to call', choices=['add', 'get', 'check', 'authenticate', 'validate', 'delete_token'])
     parser.add_argument('--user', '-u', help='Username')
     parser.add_argument('--email', '--address', '-a', nargs='+', help='Email address')
     parser.add_argument('--name', '-n', help='Full name')
@@ -53,12 +54,30 @@ if __name__ == '__main__':
                 if not args.name:
                     raise ValueError("Full name is required")
                 user = User(args.user, args.email, args.name, Role[args.role.upper()], args.password)
-                print(user_db.add_user(user))
+                response = user_db.add_user(user)
+                print("User added successfully" if response is None else response)
             case 'get':
                 print(user_db.get_user(ids[0]))
             case 'check':
                 credentials = Credentials(ids[0], args.password)
                 print(user_db.check_password(credentials))
+            case 'authenticate':
+                credentials = Credentials(ids[0], args.password)
+                response = user_auth.authenticate(credentials)
+                print(response)
+                if response is not None:
+                    user_db.store_token(response) # When authenticated the new token is stored and will be used for further requests
+                    user_auth.store_token(response)
+                    with open(token_path, 'w') as f:
+                        f.write(serialize(response)) # Save the token to a file, overwriting the previous one if present
+                        print(f'Token saved to {token_path}')
+            case 'validate':
+                print("Given token is valid" if user_auth.validate_token(token) else "Given token is invalid")
+            case 'delete_token':
+                if os.path.exists(token_path):
+                    os.remove(token_path)
+                else:
+                    print("Error: couldn't find the token file")
             case _:
                 raise ValueError(f"Invalid command '{args.command}'")
     except RuntimeError as e:
