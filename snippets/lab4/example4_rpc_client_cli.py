04894bf0abadef5f7aeea38c23d1837e3ed7761b
Sabuh <mastersabuh@gmail.com>
2024-12-01 16:27:42 +0100

    Ex4 - added cli commands to provide token duration and token paths

--- snippets/lab4/example4_rpc_client_cli.py
+++ snippets/lab4/example4_rpc_client_cli.py
@@ -1,7 +1,22 @@
 from .example3_rpc_client import *
 import argparse
 import sys
+import os
 
+def parse_timedelta(value):
+    try:
+        days = hours = minutes = 0
+        if 'd' in value:
+            days, value = value.split('d')
+            days = int(days)
+        if 'h' in value:
+            hours, value = value.split('h')
+            hours = int(hours)
+        if 'm' in value:
+            minutes = int(value.split('m')[0])
+        return timedelta(days=days, hours=hours, minutes=minutes)
+    except Exception as e:
+        raise argparse.ArgumentTypeError(f"Invalid timedelta format: {value}. Expected something like '1d2h30m'.")
 
 if __name__ == '__main__':
 
@@ -11,12 +26,18 @@ if __name__ == '__main__':
         exit_on_error=False,
     )
     parser.add_argument('address', help='Server address in the form ip:port')
-    parser.add_argument('command', help='Method to call', choices=['add', 'get', 'check'])
+    parser.add_argument('command', help='Method to call', choices=['add', 'get', 'check', 'authenticate', 'validate'])
     parser.add_argument('--user', '-u', help='Username')
     parser.add_argument('--email', '--address', '-a', nargs='+', help='Email address')
     parser.add_argument('--name', '-n', help='Full name')
     parser.add_argument('--role', '-r', help='Role (defaults to "user")', choices=['admin', 'user'])
     parser.add_argument('--password', '-p', help='Password')
+
+    parser.add_argument('--duration', '-d', type=parse_timedelta, help="Token duration formatted as (\d+d)?(\d+h)?(\dm)?")
+    parser.add_argument('--save_token_path', '-s', default="./snippets/lab4/gen_token/", help="Path to save token json file")
+    parser.add_argument('--load_token_path', '-t', default="./snippets/lab4/gen_token/token.json", help="Path to load token json file")
+
+    
 
     if len(sys.argv) > 1:
         args = parser.parse_args()
@@ -26,6 +47,7 @@ if __name__ == '__main__':
 
     args.address = address(args.address)
     user_db = RemoteUserDatabase(args.address)
+    user_auth_service = RemoteAuthenticationService(args.address)
 
     try :
         ids = (args.email or []) + [args.user]
@@ -44,7 +66,25 @@ if __name__ == '__main__':
             case 'check':
                 credentials = Credentials(ids[0], args.password)
                 print(user_db.check_password(credentials))
-            case _:
+            case 'authenticate':
+                if not args.duration:
+                    raise ValueError("Token duration is required")
+                credentials = Credentials(ids[0], args.password)
+                token_obj = user_auth_service.authenticate(credentials, args.duration)  
+                token_ast_json = serialize(token_obj)
+                #save json file to current directory
+                with open(args.save_token_path + "token.json", "w") as f:
+                    f.write(token_ast_json)
+            case 'validate':
+                if not os.path.exists(args.load_token_path):
+                    raise FileNotFoundError(f"The file {args.load_token_path} does not exist.")
+
+                with open(args.load_token_path, "r") as f:
+                    token_ast_json = f.read()
+
+                token_obj = deserialize(token_ast_json)
+                print(user_auth_service.validate_token(token_obj))
+            case _: 
                 raise ValueError(f"Invalid command '{args.command}'")
     except RuntimeError as e:
         print(f'[{type(e).__name__}]', *e.args)
