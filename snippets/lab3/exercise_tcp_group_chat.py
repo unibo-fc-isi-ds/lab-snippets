92329d4d87cd040af84f8288578f0fe715e2707a
Pengyue Xu <pengyue.xu@studio.unibo.it>
2025-11-23 22:11:38 +0100

    add thread lock to set

--- snippets/lab3/exercise_tcp_group_chat.py
+++ snippets/lab3/exercise_tcp_group_chat.py
@@ -1,5 +1,6 @@
 import sys
 from snippets.lab3 import *
+peers_lock = threading.Lock()
 # imitate the attribute peers in the lab2 example
 peers: set[Connection] = set()
 
@@ -20,7 +21,8 @@ def on_message_received(event, payload, connection, error):
             print(payload)
         case 'close':
             print(f"Connection with peer {connection.remote_address} closed")
-            peers.discard(connection)
+            with peers_lock:
+                peers.discard(connection)
         case 'error':
             print(error)
 
@@ -32,20 +34,22 @@ def on_new_connection(event, connection, address, error):
         case 'connect':
             print(f"Open ingoing connection from: {address}")
             connection.callback = on_message_received
-            peers.add(connection)
+            with peers_lock:
+                peers.add(connection)
         case 'stop':
             print(f"Stop listening for new peers")
         case 'error':
             print(error)
 
 port = int(sys.argv[1])
-remote_endpoints: list[str] = sys.argv[2:]
+remote_endpoint_list: list[str] = sys.argv[2:]
 server = Server(port, on_new_connection)
 
-for remote_endpoint in remote_endpoints:
+for remote_endpoint in remote_endpoint_list:
     peer = Client(address(remote_endpoint), on_message_received)
     print(f"Connected to {peer.remote_address}")
-    peers.add(peer)
+    with peers_lock:
+        peers.add(peer)
 
 username = input('Enter your username to start the chat:\n')
 print('Type your message and press Enter to send it. Messages from other peers will be displayed below.')
@@ -55,6 +59,7 @@ while True:
         send_group_message(content, username)
     except (EOFError, KeyboardInterrupt):
         for peer in peers:
-            peer.close()
+            with peers_lock:
+                peer.close()
         break
 server.close()
