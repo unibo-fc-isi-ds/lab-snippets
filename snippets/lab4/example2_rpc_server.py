4f34f52e5413365f7fd7030cfcb1ecd10904b427
Jiekai Sun <jiekai.sun@studio.unibo.it>
2025-11-30 11:23:06 +0100

    feat: add routing table and integrate authentication in server stub

--- snippets/lab4/example2_rpc_server.py
+++ snippets/lab4/example2_rpc_server.py
@@ -1,5 +1,5 @@
 from snippets.lab3 import Server
-from snippets.lab4.users.impl import InMemoryUserDatabase
+from snippets.lab4.users.impl import InMemoryUserDatabase, InMemoryAuthenticationService
 from snippets.lab4.example1_presentation import serialize, deserialize, Request, Response
 import traceback
 
@@ -8,6 +8,14 @@ class ServerStub(Server):
     def __init__(self, port):
         super().__init__(port, self.__on_connection_event)
         self.__user_db = InMemoryUserDatabase()
+        self.__auth_service = InMemoryAuthenticationService(self.__user_db)
+        self.__routes = {
+            "add_user": self.__user_db.add_user,
+            "get_user": self.__user_db.get_user,
+            "check_password": self.__user_db.check_password,
+            "authenticate": self.__auth_service.authenticate,
+            "validate_token": self.__auth_service.validate_token,
+        }
     
     def __on_connection_event(self, event, connection, address, error):
         match event:
@@ -38,7 +46,7 @@ class ServerStub(Server):
     
     def __handle_request(self, request):
         try:
-            method = getattr(self.__user_db, request.name)
+            method = self.__routes.get(request.name)
             result = method(*request.args)
             error = None
         except Exception as e:
