3c2235f2dba5f9e51c313fb593ec032c4a2a5bc3
Jiekai Sun <jiekai.sun@studio.unibo.it>
2025-12-01 17:21:54 +0100

    save auth token locally and validate via file path

--- snippets/lab4/example4_rpc_client_cli.py
+++ snippets/lab4/example4_rpc_client_cli.py
@@ -1,3 +1,4 @@
+import json
 from .example3_rpc_client import *
 import argparse
 import sys
@@ -17,8 +18,7 @@ if __name__ == '__main__':
     parser.add_argument('--name', '-n', help='Full name')
     parser.add_argument('--role', '-r', help='Role (defaults to "user")', choices=['admin', 'user'])
     parser.add_argument('--password', '-p', help='Password')
-    parser.add_argument('--signature', '-s', help='Signature')
-    parser.add_argument('--expiration', help='Expiration (YYYY-MM-DDTHH:MM:SS)')
+    parser.add_argument('--token', '-t', help='Token path')
 
     if len(sys.argv) > 1:
         args = parser.parse_args()
@@ -51,11 +51,18 @@ if __name__ == '__main__':
                 credentials = Credentials(ids[0], args.password)
                 token = auth_service.authenticate(credentials)
                 print(token)
+                token_file = "token.json"
+                with open(token_file, "w") as f:
+                    f.write(serialize(token))
+                print(f"Token saved to: {token_file}")
             case 'validate':
-                user = user_db.get_user(ids[0])
-                token = Token(user, datetime.fromisoformat(args.expiration), args.signature)
+                if not args.token:
+                    raise ValueError("Token file path is required via --token")
+                with open(args.token, "r") as f:
+                    raw_json = f.read()
+                token = deserialize(raw_json)
                 is_token_valid = auth_service.validate_token(token)
-                print(is_token_valid)
+                print(f"Token valid: {'YES' if is_token_valid else 'NO'}")
             case _:
                 raise ValueError(f"Invalid command '{args.command}'")
     except RuntimeError as e:
