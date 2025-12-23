45a07790b10fbc980fc4bed40909b66a2d548490
Elisa Yan <elisa.yan@studio.unibo.it>
2025-12-23 17:10:16 +0100

    feat: implement authentication methods in RemoteAuthenticationService

--- snippets/lab4/example3_rpc_client.py
+++ snippets/lab4/example3_rpc_client.py
@@ -43,6 +43,17 @@ class RemoteUserDatabase(ClientStub, UserDatabase):
         return self.rpc('check_password', credentials)
 
 
+class RemoteAuthenticationService(ClientStub, AuthenticationService):
+    def __init__(self, server_address):
+        super().__init__(server_address)
+
+    def authenticate(self, credentials: Credentials, duration=None) -> Token:
+        return self.rpc('authenticate', credentials, duration)
+
+    def validate_token(self, token: Token) -> bool:
+        return self.rpc('validate_token', token)
+
+
 if __name__ == '__main__':
     from snippets.lab4.example0_users import gc_user, gc_credentials_ok, gc_credentials_wrong
     import sys
