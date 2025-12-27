1c9b910075f1ce072e58ef719e6c5de0a771472e
mtttia <mattia.senni.03@gmail.com>
2025-12-27 10:42:36 +0100

    added EXIT_MESSAGE and script description

--- snippets/lab3/exercise_tcp_group_chat.py
+++ snippets/lab3/exercise_tcp_group_chat.py
@@ -5,19 +5,37 @@ import sys
 How to execute:
 poetry run python snippets/lab3/exercise_tcp_group_chat.py port [HOST] [HOST2] [...]
 example:
-first host: 
 first host: poetry run python snippets/lab3/exercise_tcp_group_chat.py 8080
 second host: poetry run python snippets/lab3/exercise_tcp_group_chat.py 8081 127.0.0.1:8080
-third host: poetry run python snippets/lab3/exercise_tcp_group_chat.py 8082 127.0.0.1:8080
+third host: poetry run python snippets/lab3/exercise_tcp_group_chat.py 8082 127.0.0.1:8080 127.0.0.1:8081
+
+exercise description:
+keep all connections in a connections set, once the script start it try to connect to all the passed 
+hosts.
+Then it ask for a username to use in the chat and keep it in a global variable.
+Now it begin a while true loop where a user is asked to insert a message and, if the message is not an
+empty message it broadcast it to all other users.
+Once the application handle a program interruption exception it send an exit message and it close all the
+active connection and the server listener.
+methods:
+- broadcast: is used to broadcast a message, given the message to send and the sender username
+it send the message to all the Clients in the connections list, if a send fails, it remove the hots 
+from the connections list
+- on_message_received: this is the message receiver callback, once a message is received it check for the
+event type and if is a message it print it in the chat else if it is a 'close' or a 'error' event
+id discard the client from the connections set.
+- on_new_connection: this is the callback called once a new host try to connect to the active server.
+once a new connection arrives it set as connection callback the 'on message received' function and it add
+the client to the connections list
 """
 
 connections: set[Client] = set()
 username: str | None = None
 
-def broadcast(msg: str, sender: str, origin=None):
+EXIT_MESSAGE = "<LEAVES THE CHAT>"
+
+def broadcast(msg: str, sender: str):
     for peer in list(connections):
-        if peer is origin:
-            continue
         try:
             peer.send(message(msg, sender))
         except Exception:
@@ -29,7 +47,6 @@ def on_message_received(event, payload, connection, error):
         case 'message':
             print(payload)
         case 'close':
-            print(f"Peer {connection.remote_address} disconnected")
             connections.discard(connection)
         case 'error':
             print(error)
@@ -74,6 +91,7 @@ def main():
                 broadcast(content, username)
     except (EOFError, KeyboardInterrupt):
         print("\nLeaving chat")
+        broadcast(EXIT_MESSAGE, username)
     finally:
         for peer in list(connections):
             peer.close()
