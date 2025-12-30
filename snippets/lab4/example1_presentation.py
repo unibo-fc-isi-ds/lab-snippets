c4211fd5686db24ecf88c319fbb354af124ac216
sceredi <ceredi.simone.iti@gmail.com>
2025-12-30 16:01:25 +0100

    feat: rpc auth service 1 exercise

--- snippets/lab4/example1_presentation.py
+++ snippets/lab4/example1_presentation.py
@@ -1,7 +1,8 @@
-from .users import User, Credentials, Token, Role
-from datetime import datetime
 import json
 from dataclasses import dataclass
+from datetime import datetime, timedelta
+
+from .users import Credentials, Role, Token, User
 
 
 @dataclass
@@ -47,51 +48,56 @@ class Serializer:
         if isinstance(obj, dict):
             return {key: self._to_ast(value) for key, value in obj.items()}
         # selects the appropriate method to convert the object to AST via reflection
-        method_name = f'_{type(obj).__name__.lower()}_to_ast'
+        method_name = f"_{type(obj).__name__.lower()}_to_ast"
         if hasattr(self, method_name):
             data = getattr(self, method_name)(obj)
-            data['$type'] = type(obj).__name__
+            data["$type"] = type(obj).__name__
             return data
         raise ValueError(f"Unsupported type {type(obj)}")
 
     def _user_to_ast(self, user: User):
         return {
-            'username': self._to_ast(user.username),
-            'emails': [self._to_ast(email) for email in user.emails],
-            'full_name': self._to_ast(user.full_name),
-            'role': self._to_ast(user.role),
-            'password': self._to_ast(user.password),
+            "username": self._to_ast(user.username),
+            "emails": [self._to_ast(email) for email in user.emails],
+            "full_name": self._to_ast(user.full_name),
+            "role": self._to_ast(user.role),
+            "password": self._to_ast(user.password),
         }
 
     def _credentials_to_ast(self, credentials: Credentials):
         return {
-            'id': self._to_ast(credentials.id),
-            'password': self._to_ast(credentials.password),
+            "id": self._to_ast(credentials.id),
+            "password": self._to_ast(credentials.password),
         }
 
     def _token_to_ast(self, token: Token):
         return {
-            'signature': self._to_ast(token.signature),
-            'user': self._to_ast(token.user),
-            'expiration': self._to_ast(token.expiration),
+            "signature": self._to_ast(token.signature),
+            "user": self._to_ast(token.user),
+            "expiration": self._to_ast(token.expiration),
         }
 
     def _datetime_to_ast(self, dt: datetime):
-        raise NotImplementedError("Missing implementation for datetime serialization")
+        return {"datetime": self._to_ast(dt.isoformat())}
+
+    def _timedelta_to_ast(self, td: timedelta):
+        return {"timedelta": self._to_ast(td.total_seconds())}
 
     def _role_to_ast(self, role: Role):
-        return {'name': role.name}
+        return {"name": role.name}
 
     def _request_to_ast(self, request: Request):
         return {
-            'name': self._to_ast(request.name),
-            'args': [self._to_ast(arg) for arg in request.args],
+            "name": self._to_ast(request.name),
+            "args": [self._to_ast(arg) for arg in request.args],
         }
 
     def _response_to_ast(self, response: Response):
         return {
-            'result': self._to_ast(response.result) if response.result is not None else None,
-            'error': self._to_ast(response.error),
+            "result": (
+                self._to_ast(response.result) if response.result is not None else None
+            ),
+            "error": self._to_ast(response.error),
         }
 
 
@@ -104,7 +110,7 @@ class Deserializer:
 
     def _ast_to_obj(self, data):
         if isinstance(data, dict):
-            if '$type' not in data:
+            if "$type" not in data:
                 return {key: self._ast_to_obj(value) for key, value in data.items()}
             # selects the appropriate method to convert the AST to object via reflection
             method_name = f'_ast_to_{data["$type"].lower()}'
@@ -117,42 +123,47 @@ class Deserializer:
 
     def _ast_to_user(self, data):
         return User(
-            username=self._ast_to_obj(data['username']),
-            emails=set(self._ast_to_obj(data['emails'])),
-            full_name=self._ast_to_obj(data['full_name']),
-            role=self._ast_to_obj(data['role']),
-            password=self._ast_to_obj(data['password']),
+            username=self._ast_to_obj(data["username"]),
+            emails=set(self._ast_to_obj(data["emails"])),
+            full_name=self._ast_to_obj(data["full_name"]),
+            role=self._ast_to_obj(data["role"]),
+            password=self._ast_to_obj(data["password"]),
         )
 
     def _ast_to_credentials(self, data):
         return Credentials(
-            id=self._ast_to_obj(data['id']),
-            password=self._ast_to_obj(data['password']),
+            id=self._ast_to_obj(data["id"]),
+            password=self._ast_to_obj(data["password"]),
         )
 
     def _ast_to_token(self, data):
         return Token(
-            signature=self._ast_to_obj(data['signature']),
-            user=self._ast_to_obj(data['user']),
-            expiration=self._ast_to_obj(data['expiration']),
+            signature=self._ast_to_obj(data["signature"]),
+            user=self._ast_to_obj(data["user"]),
+            expiration=self._ast_to_obj(data["expiration"]),
         )
 
     def _ast_to_datetime(self, data):
-        raise NotImplementedError("Missing implementation for datetime deserialization")
+        return datetime.fromisoformat(data["datetime"])
+
+    def _ast_to_timedelta(self, data):
+        return timedelta(seconds=self._ast_to_obj(data["timedelta"]))
 
     def _ast_to_role(self, data):
-        return Role[self._ast_to_obj(data['name'])]
+        return Role[self._ast_to_obj(data["name"])]
 
     def _ast_to_request(self, data):
         return Request(
-            name=self._ast_to_obj(data['name']),
-            args=tuple(self._ast_to_obj(arg) for arg in data['args']),
+            name=self._ast_to_obj(data["name"]),
+            args=tuple(self._ast_to_obj(arg) for arg in data["args"]),
         )
 
     def _ast_to_response(self, data):
         return Response(
-            result=self._ast_to_obj(data['result']) if data['result'] is not None else None,
-            error=self._ast_to_obj(data['error']),
+            result=(
+                self._ast_to_obj(data["result"]) if data["result"] is not None else None
+            ),
+            error=self._ast_to_obj(data["error"]),
         )
 
 
@@ -168,18 +179,24 @@ def deserialize(string):
     return DEFAULT_DESERIALIZER.deserialize(string)
 
 
-if __name__ == '__main__':
-    from snippets.lab4.example0_users import gc_user, gc_credentials_wrong
+if __name__ == "__main__":
+    from snippets.lab4.example0_users import gc_credentials_wrong, gc_user
 
     request = Request(
-        name='my_function',
+        name="my_function",
         args=(
-            gc_credentials_wrong, # an instance of Credentials
-            gc_user, # an instance of User
-            ["a string", 42, 3.14, True, False], # a list, containing various primitive types
-            {'key': 'value'}, # a dictionary
-            Response(None, 'an error'), # a Response, which contains a None field
-        )
+            gc_credentials_wrong,  # an instance of Credentials
+            gc_user,  # an instance of User
+            [
+                "a string",
+                42,
+                3.14,
+                True,
+                False,
+            ],  # a list, containing various primitive types
+            {"key": "value"},  # a dictionary
+            Response(None, "an error"),  # a Response, which contains a None field
+        ),
     )
     serialized = serialize(request)
     print("Serialized", "=", serialized)
